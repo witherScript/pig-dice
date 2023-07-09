05a390715b5cc1a68ed86e6390692daa0c01d384
Genesis Scott <chandlerscott123@gmail.com>
2023-07-09 17:32:17 -0500

    WIP: UI logic for updating the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,21 @@ class Player {
 class Turn {
   constructor() {
     this.score = 0;
+    this.turnOver = false;
   }
   roll() {
-    return Math.floor(Math.random() * 6) + 1;
+    const rollScore = Math.floor(Math.random() * 6) + 1;
+    if (rollScore !== 1) {
+      this.score += rollScore;
+      return rollScore;
+    }
+    else {
+      this.score = 0;
+      this.turnOver = true;
+    }
+  }
+  getTurnScore() {
+    return this.score;
   }
 }
 class Game {
@@ -16,7 +28,6 @@ class Game {
   constructor() {
     this.player1 = new Player();
     this.player2 = new Player();
-    this.players = [this.player1, this.player2];
 
     this.currentTurn = 1;
     this.gameOver = false;
@@ -27,22 +38,18 @@ class Game {
     this.isWinner(player);
   }
 
-  rollAgain(player) {
-    const result = player.turn.roll();
-    return result;
-  }
-
   hold(player) {
     player.score += player.turn.score;
     this.isWinner(player);
     delete player.turn;
-    this.incrementTurn();
+    this.isWinner(player) ? this.gameOver = true : this.incrementTurn();
   }
 
   handleOne(player) {
     player.turn.score = 0;
     this.incrementTurn();
     delete player.turn;
+    this.isWinner(player) ? this.gameOver = true : this.incrementTurn();
   }
 
   isWinner(player) {
@@ -50,7 +57,21 @@ class Game {
   }
 
   incrementTurn() {
-    this.currentTurn++;
+    if (this.currentTurn === 2) {
+      this.currentTurn = 1;
+    }
+    else {
+      this.currentTurn++;
+    }
+
+  }
+  whoseTurn() {
+    if (this.currentTurn % 2 === 0) {
+      return this.player2;
+    }
+    else {
+      return this.player1;
+    }
   }
 }
 
@@ -58,4 +79,59 @@ class Game {
   UI Logic
 */
 
+function populateCurrentScores(game) {
 
+  if (game.currentTurn.toString() === '1') {
+    document.getElementById('current-player').innerText = 'Player 1';
+    document.getElementById('current-player-score').innerText = game.whoseTurn().turn.score;
+
+  }
+  else {
+    document.getElementById('current-player').innerText = 'Player 2';
+    document.getElementById('current-player-score').innerText = game.whoseTurn().turn.score;
+
+  }
+
+}
+
+function unHideElement(element) {
+  element.classList.remove('hidden');
+  return true;
+}
+
+function hideElement(element) {
+  element.classList.add('hide');
+  return true;
+}
+
+function handleTurn(game) {
+  const currentTurn = game.whoseTurn();
+  game.turn(currentTurn);
+  const result = currentTurn.turn.roll();
+  if (!currentTurn.turn.turnOver) {
+    document.querySelector('span#roll-result').innerText = result;
+  }
+}
+
+function startGame() {
+  unHideElement(document.querySelector('div#game-start'));
+  const game = new Game();
+  populateCurrentScores(game);
+
+  const rollBtn = document.querySelector('button#roll');
+  const holdBtn = document.querySelector('button#hold');
+
+  rollBtn.addEventListener('click', () => handleTurn(game));
+  holdBtn.addEventListener('click', () => game.hold(game.whoseTurn()));
+  // input = button press
+  //attach button press to whoseTurn
+}
+
+
+
+window.addEventListener("load", function () {
+
+  const startBtn = document.getElementById('start');
+  startBtn.addEventListener('click', startGame);
+
+});
